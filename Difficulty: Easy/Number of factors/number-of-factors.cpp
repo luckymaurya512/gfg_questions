898f98ce9bb47e10d7c4804e230d76df20c24a5d
Lucky Maurya <118681697+luckymaurya512@users.noreply.github.com>
2024-08-22 22:43:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of factors/number-of-factors.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int countFactors(int N){
+        //code here
+        int ans=0;
+        for(int i=1;i*i<=N;i++){
+            if(N%i==0){
+                if(i==N/i) ans++;
+                else{
+                    ans+=2;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.countFactors(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
